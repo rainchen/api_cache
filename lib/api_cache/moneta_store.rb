91188e4a72619affeb4788055b6c6265ecda4ff1
RainChen <hirainchen@gmail.com>
2019-03-16 21:33:36 +0800

    fix: should be able to fetch a long url using file store

--- lib/api_cache/moneta_store.rb
+++ lib/api_cache/moneta_store.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 class APICache
   class MonetaStore < APICache::AbstractStore
     def initialize(store)
@@ -6,6 +8,7 @@ class APICache
 
     # Set value. Returns true if success.
     def set(key, value)
+      key = hash_key(key)
       @moneta[key] = value
       @moneta["#{key}_created_at"] = Time.now
       true
@@ -13,21 +16,36 @@ class APICache
 
     # Get value.
     def get(key)
+      key = hash_key(key)
       @moneta[key]
     end
 
     # Delete value.
     def delete(key)
+      key = hash_key(key)
       @moneta.delete(key)
     end
 
     # Does a given key exist in the cache?
     def exists?(key)
+      key = hash_key(key)
       @moneta.key?(key)
     end
 
     def created_at(key)
+      key = hash_key(key)
       @moneta["#{key}_created_at"]
     end
+
+    private
+
+      # use key's digest as key name to avoid error "File name too long @ rb_file_s_rename" when using File store
+      def hash_key(key)
+        if @moneta.adapter.is_a?(Moneta::Adapters::File)
+          Digest::MD5.hexdigest(key)
+        else
+          key
+        end
+      end
   end
 end
--- spec/moneta_store_spec.rb
+++ spec/moneta_store_spec.rb
@@ -7,4 +7,17 @@ describe APICache::MonetaStore do
   let(:store) { APICache::MonetaStore.new(cache) }
 
   include_examples "generic store"
+
+  it "should be able to fetch a long url using file store" do
+    url = "http://www.google.com/?q=this-is-a-#{"very"*1000}-long-string"
+    stub_request(:get, url).to_return(:body => "Google")
+
+    # store to file
+    APICache.store = Moneta.new(:File, dir: 'tmp/cache/moneta')
+
+    cache = APICache.get(url)
+    cache.should_not == nil
+    # get from store
+    APICache.get(url).should == cache
+  end
 end
